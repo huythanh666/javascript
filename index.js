91cfeb42a9437d66d442712d532add56c51392e3
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-03-16 17:19:44 +0700

    lab test

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-function repeatStringNumTimes(string,num){
-  if(num <= 0){
-    return []
+function fearNotLetter(string) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let index = alphabet.indexOf(string[0]);
+  for (let i = 0; i < string.length; i++) {
+    if (alphabet[index + i] !== string[i]) {
+      return alphabet[index + i];
+    }
   }
-  let repeat = string;
-  for(let i = 1 ; i < num ; i++){
-    console.log(string)
-    string+=repeat;
-  }
-  return string
+  return undefined;
 }
-let result = repeatStringNumTimes("*",3)
-console.log(result)
+function fearNotLetter(str) {
+  for (let i = 0; i < str.length - 1; i++) {
+    // Lấy mã Unicode của ký tự hiện tại và ký tự tiếp theo
+    let currentCode = str.charCodeAt(i);
+    let nextCode = str.charCodeAt(i + 1);
+
+    // Nếu khoảng cách giữa chúng lớn hơn 1, nghĩa là có chữ bị thiếu
+    if (nextCode - currentCode > 1) {
+      // Trả về ký tự bị thiếu đó (mã của nó là currentCode + 1)
+      return String.fromCharCode(currentCode + 1);
+    }
+  }
+  
+  return undefined;
+}
+
+console.log(fearNotLetter("stvwx")); // Trả về "u"
