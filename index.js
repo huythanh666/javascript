a717673c388a560d2d81bb7bbecda5b1f3f9bc92
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-03-19 14:46:43 +0700

    convert dna

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function sumAll(arr){
-  let newArr = arr.sort((a, b) => a - b);
-  console.log(newArr)
-  let result = 0;
-  for(let i = newArr[0]; i <= newArr[1] ; i++){
-    result += i;
+function pairElement(string){
+  let result = [];
+  for(const char of string){
+    let dna = [];
+    dna.push(char)
+    if(char === "A"){
+      dna.push("T");
+    } else if (char === "T") {
+      dna.push("A");
+    } else if (char === "C"){
+      dna.push("G")
+    } else {
+      dna.push("C")
+    }
+    result.push(dna)
   }
   return result
 }
-let result = sumAll([5, 10]);
+let result = pairElement("ATCGA");
 console.log(result)
+function pairElement(str) {
+  // Tạo bảng tra cứu các cặp base
+  const pairs = {
+    "A": "T",
+    "T": "A",
+    "C": "G",
+    "G": "C"
+  };
+
+  // Sử dụng .split().map() để tạo mảng 2 chiều nhanh chóng
+  return str.split("").map(char => [char, pairs[char]]);
+}
+
+/*
+Tiêu chí,                   Code của bạn (if...else),                   Dùng Lookup Object (map)
+Độ sạch (Clean code),       Trung bình,                                 Rất cao
+Khả năng mở rộng,           Khó (phải thêm nhiều else if),              Dễ (chỉ cần thêm key vào Object)
+Tốc độ thực thi,            Nhanh,                                      Nhanh nhất
+Độ an toàn,                 Thấp (dễ nhầm ở else),                      Cao
+*/
