367b669de451a7e15c8d84490787485d71f16328
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-03-19 14:07:34 +0700

    sum all

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-  function generatePassword(length){
-    let result = "";
-      const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
-    for(let i = 0; i < length;i++){
-      console.log(Math.floor(Math.random() * charset.length))
-const randomChar = charset.charAt(Math.floor(Math.random() * charset.length));
-      result +=randomChar
-    }
-    return result
+function sumAll(arr){
+  let newArr = arr.sort((a, b) => a - b);
+  console.log(newArr)
+  let result = 0;
+  for(let i = newArr[0]; i <= newArr[1] ; i++){
+    result += i;
   }
-
-  let password = generatePassword(7);
-  console.log(password)
-
-
-  const CHARSET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
-
-// function generatePassword(length) {
-//   let passwordArray = new Array(length); // Khởi tạo mảng với độ dài xác định
-//   const charsetLength = CHARSET.length;
-
-//   for (let i = 0; i < length; i++) {
-//     const randomIndex = Math.floor(Math.random() * charsetLength);
-//     passwordArray[i] = CHARSET[randomIndex]; // Gán trực tiếp vào chỉ số mảng
-//   }
-
-//   return passwordArray.join(''); // Nối một lần duy nhất
-// }
+  return result
+}
+let result = sumAll([5, 10]);
+console.log(result)
