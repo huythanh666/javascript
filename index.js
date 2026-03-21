23bf26e2e4a67af0159c8d1eca571bf656b90a3e
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-03-21 19:02:19 +0700

    Build a Prime Number Sum Calculator

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-function whatIsInAName(collection, source) {
-  // 1. Lấy danh sách các "key" (thuộc tính) cần kiểm tra từ đối tượng source
-  const sourceKeys = Object.keys(source);
-console.log(sourceKeys)
-  // 2. Lọc mảng collection
-  return collection.filter(obj => {
-    // 3. Kiểm tra xem MỌI key trong sourceKeys có khớp với obj hiện tại không
-    return sourceKeys.every(key => {
-      // Điều kiện: obj phải có thuộc tính 'key' và giá trị phải bằng với source
-      return obj.hasOwnProperty(key) && obj[key] === source[key];
-    });
-  });
+function sumPrimes(number){
+  let result = 0;
+  for(let i = 0; i<= number;i++){
+    if(isPrime(i)){
+        result += i
+    }
+  }
+  return result
 }
 
-// Ví dụ kiểm tra:
-const result = whatIsInAName(
-  [
-    { first: "Romeo", last: "Montague" }, 
-    { first: "Mercutio", last: null }, 
-    { first: "Tybalt", last: "Capulet" }
-  ], 
-  { last: "Capulet" }
-);
-
-console.log(result); // [{ first: "Tybalt", last: "Capulet" }]
+function isPrime(n) {
+  if (n <= 1) return false; // Số <= 1 không phải số nguyên tố
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false; // Nếu chia hết cho số nào khác, không phải số nguyên tố
+  }
+  return true;
+}
