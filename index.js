fa00f2c7680ce2f248111b702919800b678380cf
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-03-21 18:04:23 +0700

    Build a Symmetric Difference Function

--- index.js
+++ index.js
@@ -1,60 +1,12 @@
-function convertHTML(string){
-  let result = "";
-  for(const char of string){
-    if(char === "&"){
-      result += "&amp;"
-    } else if (char === "<"){
-      result += "&lt;"
-    } else if (char === ">"){
-      result += "&gt;"
-    } else if (char === `"`){
-      result += "&quot;"
-    } else if (char === "'"){
-      result += "&apos;"
-    } else {
-      result += char;
-    }
-  }
-    return result
-
+function diffArray(arr1,arr2){
+  
+    let diffArr1 =  arr1.filter((e) => {
+        return !arr2.includes(e)
+    })
+    let diffArr2 = arr2.filter((e) => {
+        return !arr1.includes(e)
+    })
+    return diffArr1.concat(diffArr2)
 }
-let result = convertHTML("Dolce & Gabbana");
+let result = diffArray(["pen", "book"], ["book", "pencil", "notebook"]); // pen pencil notebook
 console.log(result)
-
-function convertHTML(str) {
-  const htmlEntities = {
-    "&": "&amp;",
-    "<": "&lt;",
-    ">": "&gt;",
-    '"': "&quot;",
-    "'": "&apos;"
-  };
-
-  // Sử dụng Regex để tìm tất cả các ký tự đặc biệt
-  // / [&<>"'] /g có nghĩa là tìm bất kỳ ký tự nào trong ngoặc trên toàn bộ chuỗi
-  return str.replace(/[&<>"']/g, match => htmlEntities[match]);
-}
-
-
-
-function convertHTML(str) {
-  const htmlEntities = {
-    "&": "&amp;",
-    "<": "&lt;",
-    ">": "&gt;",
-    '"': "&quot;",
-    "'": "&apos;"
-  };
-  return str
-    .split("")
-    .map(char => htmlEntities[char] || char) // Nếu có trong bảng thì đổi, không thì giữ nguyên
-    .join("");
-}
-
-
-/*
-Tiêu chí,               Code của bạn (if...else),                     Dùng Regex .replace(),                          Dùng .map()
-Độ sạch code,           Trung bình,                                   Rất cao,                                        Cao
-Tốc độ thực thi,        Khá,                                          Nhanh nhất,                                     Nhanh
-Bộ nhớ,                 Trung bình,                                   Thấp,                                           Cao hơn (do tạo mảng trung gian)
-*/
