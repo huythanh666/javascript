2fb42a05258402ee6ad40193cef309a6a2c84af1
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-03-21 18:43:10 +0700

    remove item by rest

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-function diffArray(arr1,arr2){
+function destroyer(arr,...rest){
+    return arr.filter((e) => {
+        return !rest.some(number => number === e)
+    })
   
-    let diffArr1 =  arr1.filter((e) => {
-        return !arr2.includes(e)
-    })
-    let diffArr2 = arr2.filter((e) => {
-        return !arr1.includes(e)
-    })
-    return diffArr1.concat(diffArr2)
 }
-let result = diffArray(["pen", "book"], ["book", "pencil", "notebook"]); // pen pencil notebook
+
+let result = destroyer([1, 2, 3, 5, 1, 2, 3], 2,3)
+
+
+function destroyer(arr, ...rest) {
+  return arr.filter(e => !rest.includes(e));
+}
+
+function destroyer(arr, ...rest) {
+  const valuesToRemove = new Set(rest);
+  return arr.filter(e => !valuesToRemove.has(e));
+}
 console.log(result)
+
+
+/*
+
+Đặc điểm,                      filter + some (Của bạn),                 filter + includes,                                      filter + Set
+Độ phức tạp,                   O(n×m),                                   O(n×m),                                                O(n+m)
+Tốc độ (Time),                 Chậm nhất (do phải chạy callback function mỗi lần),Trung bình (nhanh hơn some một chút),         Nhanh nhất (tìm kiếm trong Set là O(1))
+Bộ nhớ (Space),                Thấp (không tạo cấu trúc dữ liệu mới),   Thấp (không tạo cấu trúc dữ liệu mới),                  Cao hơn (tốn thêm bộ nhớ để tạo Set)
+*/
