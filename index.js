9d0f65ae134249048d22b8e918ae2b36805f7b88
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-03-26 17:10:55 +0700

    switch theme light or dark

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const tabs = document.querySelectorAll('[role="tab"]');
-const panels = document.querySelectorAll('[role="tabpanel"]');
+const themes = [
+  { 
+    name: "light", 
+    message: "Light theme activated" 
+  },
+  { 
+    name: "dark", 
+    message: "Dark theme activated" 
+  }
+];
 
-tabs.forEach(tab => {
-  tab.addEventListener("click", () => {
-    tabs.forEach(t => t.setAttribute("aria-selected", "false"));
-    panels.forEach(p => p.hidden = true);
+const themeBtn = document.getElementById("theme-switcher-button");
+const themeDropdown = document.getElementById("theme-dropdown");
+const themeMessage = document.querySelector('[aria-live="polite"]');
 
-    tab.setAttribute("aria-selected", "true");
-    const associatedPanel = tab.getAttribute("aria-controls");
-    const panel = document.getElementById(associatedPanel);
-panel.hidden = false;
-  });
+const toggleMenu = () => {
+  const isExpanded = themeBtn.getAttribute("aria-expanded") === "true";
+  themeBtn.setAttribute("aria-expanded", !isExpanded);
+  themeDropdown.hidden = isExpanded;
+};
+
+themeBtn.addEventListener("click", () => toggleMenu());
+themeDropdown.addEventListener("click", (e) => {
+  const targetLi = e.target.closest('[role="menuitem"]');
+  
+  if (targetLi) {
+    const selectedName = targetLi.textContent.trim().toLowerCase();
+    const selectedTheme = themes.find(t => t.name === selectedName);
+    if (selectedTheme) {
+      document.body.className = `theme-${selectedName}`;
+      themeMessage.textContent = selectedTheme.message;
+      toggleMenu();
+    }
+  }
 });
