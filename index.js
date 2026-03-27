8777f9e47a1f2ba286d7e1da9e974aeb46bc789f
nguyenhuythanh <dev.nguyenhuythanh@gmail.com>
2026-03-27 15:37:53 +0700

    lab

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
-const themes = [
-  { 
-    name: "light", 
-    message: "Light theme activated" 
-  },
-  { 
-    name: "dark", 
-    message: "Dark theme activated" 
+const noteEl = document.getElementById("note");
+const statusEl = document.getElementById("status");
+
+let currentContent = "";
+
+noteEl.addEventListener("focus", () => {
+  statusEl.textContent = ""
+});
+
+noteEl.addEventListener("blur", () => {
+  const newContent = noteEl.innerHTML;
+
+  if (currentContent === newContent) {
+    return;
   }
-];
 
-const themeBtn = document.getElementById("theme-switcher-button");
-const themeDropdown = document.getElementById("theme-dropdown");
-const themeMessage = document.querySelector('[aria-live="polite"]');
+  currentContent = newContent;
+  console.log(currentContent);
 
-const toggleMenu = () => {
-  const isExpanded = themeBtn.getAttribute("aria-expanded") === "true";
-  themeBtn.setAttribute("aria-expanded", !isExpanded);
-  themeDropdown.hidden = isExpanded;
-};
+  statusEl.textContent = "Note saved successfully!";
+});
 
-themeBtn.addEventListener("click", () => toggleMenu());
-themeDropdown.addEventListener("click", (e) => {
-  const targetLi = e.target.closest('[role="menuitem"]');
-  
-  if (targetLi) {
-    const selectedName = targetLi.textContent.trim().toLowerCase();
-    const selectedTheme = themes.find(t => t.name === selectedName);
-    if (selectedTheme) {
-      document.body.className = `theme-${selectedName}`;
-      themeMessage.textContent = selectedTheme.message;
-      toggleMenu();
-    }
-  }
+window.addEventListener("DOMContentLoaded", () => {
+  currentContent = noteEl.textContent;
 });
